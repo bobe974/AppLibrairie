4ad24ec540de3ebc0a47d5ab88e12362d20922d0
etienneDev <etienne.baillif97436@gmail.com>
2023-12-07 14:02:32 +0400

    Configuration db H2

---
+++ src/main/resources/data/categories.sql
@@ -0,0 +1,5 @@
+INSERT INTO categorie (id, name) VALUES ('INF', 'Informatique');
+INSERT INTO categorie (id, name) VALUES ('MAT', 'Mathématiques, Physiques');
+INSERT INTO categorie (id, name) VALUES ('PHI', 'Philosophie');
+INSERT INTO categorie (id, name) VALUES ('ALB', 'Albums');
+INSERT INTO categorie (id, name) VALUES ('POL', 'Politique');
